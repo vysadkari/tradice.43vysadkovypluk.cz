0a387ea7403346d7ad9a5552b183e55f88b0a2d1
Pavel Štěrba <email@pavelsterba.com>
2023-05-06 08:19:19 +0200

    Opraveno přímé načítání podstránek

--- next.config.js
+++ next.config.js
@@ -4,6 +4,29 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  exportPathMap: async function (
+    defaultPathMap,
+    { dev, dir, outDir, distDir, buildId }
+  ) {
+    return {
+      "/": { page: "/" },
+
+      "/historie": { page: "/historie" },
+      "/historie/index": { page: "/historie" },
+
+      "/zahranicni-operace": { page: "/zahranicni-operace" },
+      "/zahranicni-operace/index": { page: "/zahranicni-operace" },
+
+      "/velitele": { page: "/velitele" },
+      "/velitele/index": { page: "/velitele" },
+
+      "/uniforma": { page: "/uniforma" },
+      "/uniforma/index": { page: "/uniforma" },
+
+      "/charon": { page: "/charon" },
+      "/charon/index": { page: "/charon" },
+    };
+  },
 };
 
 module.exports = nextConfig;
